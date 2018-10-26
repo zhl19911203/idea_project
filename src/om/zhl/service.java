54fe5ab7158880d900c0f08f1b63c6b8bd85e705
zhl <852039873@qq.com>
2018-10-26 22:23:10 +0800

    第一次提交service

---
+++ src/om/zhl/service.java
@@ -0,0 +1,12 @@
+package om.zhl;
+
+
+/**
+ * @author shkstart
+ * @create 2018-10-26 21:44
+ */
+public class service {
+    public static void main(String[] args) {
+        System.out.println("2312");
+    }
+}
